495cbfcbe8b959d9bbeb809ef909168225edca6a
Jesse Newland <jesse@jnewland.com>
2021-09-10 10:21:02 -0500

    cleanup

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e
 
 tmpdir=$(mktemp -d)
-trap "rm -rf $tmpdir" EXIT
+trap "docker compose --project-name try-shell --file $tmpdir/docker-compose.yml down; rm -rf $tmpdir" EXIT
 
 if grep -q try-shell docker-compose.yml>/dev/null; then
   cp docker-compose.yml $tmpdir/docker-compose.yml
@@ -17,7 +17,7 @@ if [ -z "${CASED_SHELL_HOSTNAME}" ]; then
 fi
 export CASED_SHELL_PORT=$(echo "${CASED_SHELL_HOSTNAME}" | cut -f 2 -d :)
 
-echo "Starting Cased Shell. Press Ctrl+C to quit" 1>&2
+echo "Starting Cased Shell. Press Ctrl+C to quit and cleanup" 1>&2
 
 docker compose --project-name try-shell --file $tmpdir/docker-compose.yml up \
   --always-recreate-deps --force-recreate --remove-orphans --renew-anon-volumes
