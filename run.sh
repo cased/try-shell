0f97641d8a01ffd43926080cba178734b142f0fc
Jesse Newland <jesse@jnewland.com>
2021-09-15 14:29:48 -0500

    cleanup

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ set -e
 tmpdir=$(mktemp -d)
 trap "docker compose --project-name try-shell --file $tmpdir/docker-compose.yml down; rm -rf $tmpdir" EXIT
 
-if grep -q try-shell docker-compose.yml>/dev/null; then
+if grep -q try-shell docker-compose.yml 2>/dev/null; then
   cp docker-compose.yml $tmpdir/docker-compose.yml
 else
-  curl https://github.com/cased/try-shell/raw/main/docker-compose.yml > $tmpdir/docker-compose.yml
+  curl -s https://raw.githubusercontent.com/cased/try-shell/main/docker-compose.yml > $tmpdir/docker-compose.yml
 fi
 
 if [ -z "${CASED_SHELL_HOSTNAME}" ]; then
