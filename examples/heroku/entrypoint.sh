d4c0d59be5aded3bc427c13fcd21a0ed705794cd
Jesse Newland <jesse@jnewland.com>
2021-11-17 10:55:03 -0600

    heroku@localhost

--- examples/heroku/entrypoint.sh
+++ examples/heroku/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+/usr/sbin/sshd &
+
 # Configure for Heroku
 export CASED_SHELL_PORT=$PORT
 export CASED_SHELL_TLS=off
 export CASED_SHELL_HOSTNAME=$HEROKU_APP_NAME.herokuapp.com
 : ${CASED_SHELL_LOG_LEVEL:="error"}
+export CASED_SHELL_SSH_PRIVATE_KEY="$(cat /home/heroku/.ssh/id_rsa)"
 
-python -u run.py --logging=$CASED_SHELL_LOG_LEVEL
+python -u run.py --logging=$CASED_SHELL_LOG_LEVEL &
+
+wait -n
