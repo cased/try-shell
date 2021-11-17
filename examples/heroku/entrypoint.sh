72b043956d15be428de31a7815dc961a5711bf7f
Jesse Newland <jesse@jnewland.com>
2021-11-17 10:59:04 -0600

    keygen

--- examples/heroku/entrypoint.sh
+++ examples/heroku/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+su - heroku -c "ssh-keygen -P '' -f /home/heroku/.ssh/id_rsa; \
+                cp /home/heroku/.ssh/id_rsa.pub /home/heroku/.ssh/authorized_keys"
+
 /usr/sbin/sshd &
 
 # Configure for Heroku
