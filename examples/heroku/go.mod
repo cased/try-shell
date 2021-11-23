abe37b104f08a558cf0e17c11c4f58812e9e7ecd
Jesse Newland <jesse@jnewland.com>
2021-11-22 22:02:50 -0600

    ssh cert validationn

--- examples/heroku/go.mod
+++ examples/heroku/go.mod
@@ -1,15 +1,14 @@
 require (
 	github.com/creack/pty v1.1.17
 	github.com/gliderlabs/ssh v0.3.3
-  github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
-	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871
 )
 
-module github.com/cased/try-cased/examples/heroku
+require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+)
+
+module github.com/cased/try-ssh/examples/heroku
 
 go 1.17
--- examples/heroku/ssh.go
+++ examples/heroku/ssh.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"syscall"
+	"time"
 	"unsafe"
 
 	"github.com/creack/pty"
 	"github.com/gliderlabs/ssh"
+
+	gossh "golang.org/x/crypto/ssh"
+)
+
+const (
+	shellUrl = "http://localhost:8888"
 )
 
 func setWinsize(f *os.File, w, h int) {
@@ -18,26 +27,82 @@ func setWinsize(f *os.File, w, h int) {
 		uintptr(unsafe.Pointer(&struct{ h, w, x, y uint16 }{uint16(h), uint16(w), 0, 0})))
 }
 
-func main() {
-	s := &ssh.Server{
-		Addr: "127.0.0.1:2224",
-		PublicKeyHandler: func(ctx ssh.Context, key ssh.PublicKey) bool {
-			return true
-		},
-		PasswordHandler: func(ctx ssh.Context, password string) bool {
-			return true
-		},
+func casedShellIsUserAuthority(providedPubKey gossh.PublicKey) bool {
+	resp, err := http.Get(fmt.Sprintf("%s/ca.pub", shellUrl))
+	if err != nil || resp.StatusCode != 200 {
+		log.Println("error contacting shell")
+		return false
+	}
+	authorizedKeyString, _ := io.ReadAll(resp.Body)
+	authorizedKey, _, _, _, err := gossh.ParseAuthorizedKey([]byte(authorizedKeyString))
+	if err != nil {
+		log.Println(fmt.Sprintf("Failed to parse ca.pub: %s %v", authorizedKeyString, err))
+		return false
+	}
+	return bytes.Equal(providedPubKey.Marshal(), authorizedKey.Marshal())
+}
+
+func casedShellPublicKeyHandler(ctx ssh.Context, pubKey ssh.PublicKey) bool {
+	cert, ok := pubKey.(*gossh.Certificate)
+	if !ok {
+		log.Println("normal key pairs not accepted")
+		return false
+	}
+	if cert.CertType != gossh.UserCert {
+		log.Printf("cert has type %d\n", cert.CertType)
+		return false
+	}
+	c := &gossh.CertChecker{
+		IsUserAuthority: casedShellIsUserAuthority,
 	}
 
-	s.Handle(func(s ssh.Session) {
-		cmd := exec.Command("bash", "--rcfile", "/heroku-bashrc")
+	if !c.IsUserAuthority(cert.SignatureKey) {
+		log.Println("certificate signed by unrecognized authority")
+		return false
+	}
+
+	resp, err := http.Get(fmt.Sprintf("%s/principal.txt", shellUrl))
+	if err != nil || resp.StatusCode != 200 {
+		log.Println("error contacting shell")
+		return false
+	}
+	principal, _ := io.ReadAll(resp.Body)
+
+	if err := c.CheckCert(string(principal), cert); err != nil {
+		log.Printf("%s not in list of valid principals %v\n", string(principal), cert.ValidPrincipals)
+		return false
+	}
+
+	if cert.Permissions.CriticalOptions["force-command"] != "" {
+		log.Printf("invalid force-command: %s\n", cert.Permissions.CriticalOptions["force-command"])
+		return false
+	}
+
+	log.Printf("accepted SSH Certificate from %s\n", cert.ValidPrincipals[0])
+
+	return true
+}
+
+func casedShellSessionHandler(command []string) ssh.Handler {
+	return func(s ssh.Session) {
+		log.Printf("accepted connection for user %s\n", s.User())
+		if len(s.Command()) > 0 {
+			log.Println("command execution not supported")
+			io.WriteString(s, "command execution not supported\n")
+			s.Exit(1)
+			return
+		}
+		log.Println("starting shell")
+		cmd := exec.Command(command[0], command[1:]...)
 		ptyReq, winCh, isPty := s.Pty()
 		if isPty {
 			cmd.Env = append(cmd.Env, fmt.Sprintf("TERM=%s", ptyReq.Term))
 			cmd.Env = append(cmd.Env, "SHELL=/bin/bash")
 			f, err := pty.Start(cmd)
 			if err != nil {
-				panic(err)
+				io.WriteString(s, "error starting PTY\n")
+				s.Exit(1)
+				return
 			}
 			go func() {
 				for win := range winCh {
@@ -45,15 +110,25 @@ func main() {
 				}
 			}()
 			go func() {
-				io.Copy(f, s) // stdin
+				io.Copy(f, s)
 			}()
-			io.Copy(s, f) // stdout
+			io.Copy(s, f)
 			cmd.Wait()
 		} else {
 			io.WriteString(s, "No PTY requested.\n")
 			s.Exit(1)
 		}
-	})
+	}
+}
+
+func main() {
+	s := &ssh.Server{
+		Addr:             ":2224",
+		PublicKeyHandler: casedShellPublicKeyHandler,
+		IdleTimeout:      60 * time.Second,
+	}
+
+	s.Handle(casedShellSessionHandler([]string{"bash", "--rcfile", "/heroku-bashrc"}))
 
 	log.Println("starting ssh server on port 2224...")
 	log.Fatal(s.ListenAndServe())
