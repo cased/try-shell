e3677b0fd817a08a44f19eec965636fa33726bb4
Jesse Newland <jesse@jnewland.com>
2021-11-23 12:47:33 -0600

    set CASED_SHELL_OAUTH_UPSTREAM

--- examples/heroku/entrypoint.sh
+++ examples/heroku/entrypoint.sh
@@ -5,6 +5,7 @@ export CASED_SHELL_PORT=$PORT
 export CASED_SHELL_TLS=off
 export CASED_SHELL_HOSTNAME=$HEROKU_APP_NAME.herokuapp.com
 : ${CASED_SHELL_LOG_LEVEL:="error"}
+export CASED_SHELL_OAUTH_UPSTREAM=localhost:2225
 
 /bin/heroku-ssh http://localhost:$PORT &
 python -u run.py --logging=$CASED_SHELL_LOG_LEVEL &
